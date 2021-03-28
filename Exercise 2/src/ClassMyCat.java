75a8401f270eb835f3e02f1ddf6df4ef09e3e0d0
Tan Yee Jin 279890 <80802314+TanYeeJin-279890@users.noreply.github.com>
2021-03-28 12:55:27 +0800

    Exercise 2

---
+++ Exercise 2/src/ClassMyCat.java
@@ -0,0 +1,16 @@
+
+public class ClassMyCat {
+	
+    String name = "Cola.";
+    String colour = "Orange";
+    double weight =  2.50;
+    int age = 1;
+    
+    void HasShots () {
+   	 System.out.println("Has Shots");
+    }
+    
+
+}
+
+
---
+++ Exercise 2/src/Main.java
@@ -0,0 +1,57 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ClassCat a = new ClassCat();
+		System.out.println(a.name);
+		
+		ClassCat b = new ClassCat();
+		System.out.println(b.colour);
+		
+		ClassCat c = new ClassCat();
+		System.out.println(c.weight + "kg");
+		
+		ClassCat d = new ClassCat();
+		System.out.println(d.age + "years old");
+		
+		ClassCat e = new ClassCat();
+		e.HasShots();
+		
+		System.out.println();
+		
+		ClassMyCat f = new ClassMyCat();
+		System.out.println(f.name);
+		
+		ClassMyCat g = new ClassMyCat();
+		System.out.println(g.colour + " in colour");
+		
+		ClassMyCat h = new ClassMyCat();
+		System.out.println(h.weight + "kg");
+		
+		ClassMyCat i = new ClassMyCat();
+		System.out.println(i.age + "year old");
+		
+		ClassMyCat j = new ClassMyCat();
+		j.HasShots();
+		
+        System.out.println();
+		
+		ClassAbuCat k = new ClassAbuCat();
+		System.out.println(k.name);
+		
+		ClassAbuCat l = new ClassAbuCat();
+		System.out.println(l.colour + " in colour");
+		
+		ClassAbuCat m = new ClassAbuCat();
+		System.out.println(m.weight + "kg");
+		
+		ClassAbuCat n = new ClassAbuCat();
+		System.out.println(n.age + "year old");
+		
+		ClassAbuCat o = new ClassAbuCat();
+		o.HasShots();
+
+	}
+
+}
