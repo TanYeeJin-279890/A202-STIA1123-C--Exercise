51b504fe972542b620b6d3c34f402d2ae3b668b4
Tan Yee Jin 279890 <80802314+TanYeeJin-279890@users.noreply.github.com>
2021-03-28 12:34:53 +0800

    Ex 2

---
+++ Ex 2/src/ClassAbuCat.java
@@ -0,0 +1,13 @@
+
+public class ClassAbuCat {
+	
+	   String name = "Lina.";
+	   String colour = "Chocolate";
+	   double weight =  3.00;
+	   int age = 2;
+	    
+	   void HasShots () {
+	   	 System.out.println("Has Shots");
+	    }
+
+}
