c212a5bda30253e3c49737b2cc9dc37946cb3f32
Tan Yee Jin 279890 <80802314+TanYeeJin-279890@users.noreply.github.com>
2021-04-13 21:25:23 +0800

    Update Main.java

--- Exercise 2.4/src/Main.java
+++ Exercise 2.4/src/Main.java
@@ -11,6 +11,7 @@ public class Main {
 		a.setwidth(2.96);
 		a.setdepth(0.33);
 		a.setprice(639.00);
+		a.setNoOfPhone(2);
 		
 		System.out.println("Brand > " + a.getbrand());
 		System.out.println("Colour > " + a.getcolour());
@@ -19,11 +20,13 @@ public class Main {
 		System.out.println("Price > RM " + a.getPrice());
 		System.out.print("Dimension : Height > " + a.getheight() + " mm, ");
 		System.out.print("Width > " + a.getwidth() + " mm, ");
-		System.out.print("Depth > " + a.getDepth() + " mm\n\n");
+		System.out.print("Depth > " + a.getDepth() + " mm\n");
+		System.out.print("Total price for "+ a.getNoOfPhone() +" "+ a.getbrand() + " is > RM " + a.gettotalprice());
+		
 		
 
 		
-		smartphone b = new smartphone();
+		phone b = new phone();
 		b.setBrand("HONOR 20");
 		b.setModel("YAL-L21");
 		b.setColour("Midnight Black, Sapphire Blue, Icelandic white, Phantom Blue");
@@ -32,17 +35,20 @@ public class Main {
 		b.setwidth(2.91);
 		b.setdepth(0.31);
 		b.setprice(1200.00);
+		b.setNoOfPhone(3);
 		
-		System.out.println("Brand > " + b.getbrand());
+		System.out.println("\n\nBrand > " + b.getbrand());
 		System.out.println("Colour > " + b.getcolour());
 		System.out.println("Model > " + b.getmodel());
 		System.out.println("Weight > " + b.getweight() + " g");
 		System.out.println("Price > RM " + b.getPrice());
 		System.out.print("Dimension : Height > " + b.getheight() + " mm, ");
 		System.out.print("Width > " + b.getwidth() + " mm, ");
-		System.out.print("Depth > " + b.getDepth() + " mm");
+		System.out.print("Depth > " + b.getDepth() + " mm\n");
+		System.out.println("Total price for "+ b.getNoOfPhone() +" "+ b.getbrand() + " is > RM " + b.gettotalprice());
 		
 
 	}
+
 
 }
