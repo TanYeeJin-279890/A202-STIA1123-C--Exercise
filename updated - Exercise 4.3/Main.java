bf52d3cc6131b7081a4857727c2c15e190695b8b
Tan Yee Jin 279890 <80802314+TanYeeJin-279890@users.noreply.github.com>
2021-05-04 18:18:18 +0800

    Exercise 4.3 - updated

---
+++ updated - Exercise 4.3/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Grape g = new Grape("Grape",2);
+		System.out.println(g);//print using overriding method
+		System.out.println();
+		
+		Mango m = new Mango("Mango",3);
+		System.out.println(m);//print using overriding method
+		System.out.println();
+		
+		Apple a = new Apple("Apple","Red,Green",1.80);
+		System.out.println(a);//print using overriding method
+		System.out.println();
+
+
+		RedApple pl = new RedApple("Pink Lady" , "Red" ,1.80 ,  "Cripps Pink" , "Western Australia" , "November to August" , 133.0);
+		System.out.println(pl);//print using overriding method
+		System.out.println();
+		
+		GreenApple gs = new GreenApple("Granny Smith" , "Green" , 1.54 , "No retail name" , "US" , "beginning in August and are available year round" , 128.0);
+		System.out.println(gs);//print using overriding method
+		System.out.println();
+
+		
+		System.out.println("------------------------Thankyou-----------------------");
+		
+		}
+
+}
+
+
